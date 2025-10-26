4b198561de8f931dae1aeb8f42ec1f1562853db9
Dihinii <dihininulanga@gmail.com>
2025-10-26 21:54:38 +0530

    Added all function declarations
    Added all function declarations

--- main.c
+++ main.c
@@ -8,6 +8,18 @@
 #define FUEL_PRICE 310.0
 
 
+void addCity(char cities[MAX_CITIES][MAX_NAME_LENGTH],int *cityCount);
+void renameCity(char cities[MAX_CITIES][MAX_NAME_LENGTH],int cityCount);
+void removeCity(char cities[MAX_CITIES][MAX_NAME_LENGTH],int *cityCount);
+void displayCity(char cities[MAX_CITIES][MAX_NAME_LENGTH],int cityCount);
+void manageDistance(char cities[MAX_CITIES][MAX_NAME_LENGTH],float distance_matrix[MAX_CITIES][MAX_CITIES],int cityCount);
+void displayDistanceTable(char cities[MAX_CITIES][MAX_NAME_LENGTH],float distance_matrix[MAX_CITIES][MAX_CITIES],int cityCount);
+void deliveryRequest(char cities[MAX_CITIES][MAX_NAME_LENGTH], float distance_matrix[MAX_CITIES][MAX_CITIES], int *deliveryCount, int cityCount);
+void report(float distance_matrix[MAX_CITIES][MAX_CITIES], int deliverySource[MAX_DELIVERIES], int deliveryDestination[MAX_DELIVERIES], float customerCharge[MAX_DELIVERIES], float profit[MAX_DELIVERIES], float time[MAX_DELIVERIES], int deliveryCount);
+void leastDistanceRoute(char cities[MAX_CITIES][MAX_NAME_LENGTH],float distance_matrix[MAX_CITIES][MAX_CITIES],int cityCount);
+
+
+
 int main()
 {
     printf("Hello world!\n");
