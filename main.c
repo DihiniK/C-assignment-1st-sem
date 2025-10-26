c1a162fd9daaf4b9ad3524618abd1870a963adc5
Dihinii <dihininulanga@gmail.com>
2025-10-26 21:53:00 +0530

    Added define tags
    Added define tags

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_CITIES 30
+#define MAX_NAME_LENGTH 50
+#define MAX_DELIVERIES 50
+#define FUEL_PRICE 310.0
+
+
 int main()
 {
     printf("Hello world!\n");
