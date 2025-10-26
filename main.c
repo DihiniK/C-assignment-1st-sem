17c587554f8f0789eb9bf09621a2f44c7e8a465a
Dihinii <dihininulanga@gmail.com>
2025-10-26 21:57:47 +0530

    Added city data
    Added city data

--- main.c
+++ main.c
@@ -24,6 +24,12 @@ float ratePerKm[3] = {30, 40, 80};
 float avgSpeed[3] = {60, 50, 45};
 float fuelEfficiency[3] = {12, 6, 4};
 
+char cities[MAX_CITIES][MAX_NAME_LENGTH];
+int cityCount = 0;
+float distance_matrix[MAX_CITIES][MAX_CITIES];
+
+
+
 
 int main()
 {
